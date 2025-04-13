280608f448a3f8a3ce97fb2918c88f3e226a6fa2
Chimenma Goodness <59948754+switnexxtra@users.noreply.github.com>
2025-04-13 17:09:44 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,9 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 mail = Mail()  # Initialize Flask-Mail
+socketio = SocketIO(cors_allowed_origins="*")  # Allow from any frontend origin
