dbe0b1178cce7b7eab09d7d0a4088ee50bdcb355
Chimenma Goodness <59948754+switnexxtra@users.noreply.github.com>
2025-04-13 17:14:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,12 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
     # 🛠 Prevent database connection errors
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True  # Checks connection before using it
     }
+    
     
     # Mail settings
     MAIL_SERVER = 'smtp.gmail.com'
